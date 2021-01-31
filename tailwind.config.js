74cdf4824ad34ac5726ce20199c01216d715ed27
rajjo <rajjo@Rajwinderpals-MacBook-Pro.local>
2021-01-31 13:40:58 -0500

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
         primary: '#ff6363',
         secondary: {
           100: '#e2e2d5',
-          200: '#888883',
+          200: '#888882',
         },
       },
       fontFamily: {
